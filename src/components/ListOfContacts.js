4322e71e7484bfa0c4014e3a622165b5967bb5cb
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-08-27 20:34:39 +0300

    rebuild-repository---old-one-crashed

---
+++ src/components/ListOfContacts.js
@@ -0,0 +1,17 @@
+export default function ListOfContacts(contacts) {
+    return (
+      <ul>
+        {contacts.contacts.contacts.map(contItem => {
+          const { id, name, num } = contItem;
+          return (
+            <li key={id}>
+              <p>
+                {name}: {num}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+  
